8ce61ad17b130a4efe827a485fbef6ffbf6ee27f
sabinacavane <sabyna.henryck97@gmail.com>
2016-07-29 12:52:57 +0300

    SV
    Modelo e migracao de utilizadores

---
+++ database/migrations/2016_07_29_091133_utilizadores.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Utilizadores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilizadores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('senha');
+            $table->enum('privilegio',['admin','superadmin']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('utilizadores');
+    }
+}
