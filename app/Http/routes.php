621a15ccf334bcdcc8fbeaa76cda7abca96cc4ac
Edson Michaque <edsonmichaque@yandex.com>
2016-07-30 11:13:19 +0200

    Implementação de login e autenticacao via token

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,33 +10,36 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 // Routas para departamentos
-$read_write = [
-    'only' => [
-      'index',
-      'show',
-      'store',
-      'destroy',
-      'update'
-    ]
-  ];
 
   Route::post('registo', 'UtilizadorController@registo');
+  Route::group(['middleware' => 'autenticacao'], function() {
+    Route::post('login', 'SessaoController@store');
+  });
+
+  $read_write = [
+    'only' => ['index', 'show', 'store', 'destroy', 'update']
+  ];
+
+  Route::group(['middleware' => 'verificar_token:admin'], function() use ($read_write) {
+    //Routas para cursos
+    Route::resource('departamentos.cursos','CursoController', $read_write);
+
+    //Routas para Visitantes
+    Route::resource('visitantes','VisitanteController', $read_write);
+
+    //Routas para projectos
+    Route::resource('projectos','ProjectoController', $read_write);
+
+    //Routas para projectistas
+    Route::resource('projectistas','ProjectistaController', $read_write);
+
+    //Routas para criterios
+    Route::resource('criterios','CriterioController', $read_write);
+  });
+
+
+Route::group(['middleware' => 'verificar_token:superadmin'], function() use($read_write) {
   //Rotas para departamentos
   Route::resource('departamentos','DepartamentoController', $read_write);
-
-  //Routas para cursos
-  Route::resource('departamentos.cursos','CursoController', $read_write);
-
-  //Routas para Visitantes
-  Route::resource('visitantes','VisitanteController', $read_write);
-
-  //Routas para projectos
-  Route::resource('projectos','ProjectoController', $read_write);
-
-  //Routas para projectistas
-  Route::resource('projectistas','ProjectistaController', $read_write);
-
-  //Routas para criterios
-  Route::resource('criterios','CriterioController', $read_write);
+});
