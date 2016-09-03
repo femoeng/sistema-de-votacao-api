2c0351fa1a94bfbed266f90eb60c27fc456a5781
Edson Michaque <edsonmichaque@yandex.com>
2016-09-03 10:34:21 +0200

    Retornar erros mais claros durante o registo do Administrador

--- app/Http/Middleware/ValidarRegistoDeUtilizador.php
+++ app/Http/Middleware/ValidarRegistoDeUtilizador.php
@@ -19,29 +19,40 @@ class ValidarRegistoDeUtilizador
         $data = $request->json()->all();
         $utilizador_valido = true;
         $utilizador = [];
+        $erros = [];
 
         if (isset($data['nome'])) {
           $utilizador['nome'] = $data['nome'];
         } else {
           $utilizador_valido = false;
+          if (!isset($erros['nome'])) {
+            $erros['nome'] = [];
+          }
+
+          array_push($erros['nome'], 'O nome é obrigatório');
         }
 
         if (isset($data['senha'])) {
           $utilizador['senha'] = \Hash::make($data['senha']);
         } else {
           $utilizador_valido = false;
+          if (!isset($erros['senha'])) {
+            $erros['senha'] = [];
+          }
+
+          array_push($erros['senha'], 'A senha é obrigatória');
         }
 
         $utilizador['privilegio'] = 'superadmin';
-        
+
         if ($utilizador_valido) {
           $request->utilizador_data = $utilizador;
           return $next($request);
         } else {
-          abort(400);
+          return response()->json(['erros' => $erros], 400);
         }
       } else {
-        abort(403);
+        return response()->json(['erros' => ['Já existe um administrador registado']], 403);
       }
     }
 }
