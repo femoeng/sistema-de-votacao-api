76a5d02c1b41fcf2848bb7bd35b84ca9e5aa96d3
Pedro Peter <Pedro Peter>
2016-09-24 19:14:01 -0300

    correcao de erros

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -82,7 +82,7 @@ class Kernel extends HttpKernel
         'validar_voto'=> \App\Http\Middleware\ValidarVoto::class,
         'verificar_credenciais_do_visitante' => \App\Http\Middleware\AutenticarVisitante::class,
         'verificar_codigo_visitante' => \App\Http\Middleware\VerificarCodigoVisitante::class,
-        "verificar_url"=>\App\Http\Middleware\ValidarMensagem::class,
+        "validar_url"=>\App\Http\Middleware\ValidarMensagem::class,
          "validar_conteudo"=>\App\Http\Middleware\ValidarConteudoDaMensagem::class,
 
         'auth' => \App\Http\Middleware\Authenticate::class,
